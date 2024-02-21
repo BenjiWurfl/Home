6ca3e4ab451c4f6ab39ca45b573b2cd6e2724721
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2024-02-22 00:14:18 +0100

    changes undso

--- script.js
+++ script.js
@@ -8,7 +8,11 @@ import {
     getDocs,
     getFirestore,
     updateDoc,
-    getCountFromServer
+    getCountFromServer,
+    query,
+    limit,
+    where,
+    orderBy
 } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 import {getAuth, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
@@ -26,15 +30,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth();
-let notesArr = [];
-let projectsArr = []
-let currentProject;
-let currentNote;
-let currentSortByState = "Date down";
 
 onAuthStateChanged(auth, (user) => {
     if (user) {
-        loadAmounts();
+        loadDashboardData();
     } else {
         console.log("No user is signed in.");
     }
@@ -44,22 +43,123 @@ const amountOfProjects = document.querySelector('.num-of-Projects');
 const amountOfAppointments = document.querySelector('.num-of-Appointments');
 const amountOfMindmaps = document.querySelector('.num-of-Mindmaps');
 
-async function loadAmounts(){
+const projectsRef = collection(db, "users", user.uid, "projects");
+const eventsRef = collection(db, "users", user.uid, "events");
+const mindmapsRef = collection(db, "users", user.uid, "mindmaps");
+
+let projectsArr =  [];
+let eventsArr = [];
+let mindmapsArr = [];
+
+const tbodyOfProjects = document.querySelector('.tbody-projects')
+const tbodyOfEvents= document.querySelector('.tbody-events')
+const tbodyOfMindmaps= document.querySelector('.tbody-mindmaps')
+
+function loadDashboardData(){
+    loadAmounts();
+    loadRecentProjectsIntoArrays();
+    loadDataIntoDOM();
+
+}
+
+function loadDataIntoDOM(){
+    for (let i = 0; i < 3; i++){
+        fillTbodyProjects(i);
+        fillTbodyEvents(i);
+        fillTbodyMindMaps(i);
+    }
+}
+
+function fillTbodyProjects(index){
+    tbodyOfProjects.innerHTML += '<tr>\n' +
+        '                                <td class="py-2 px-4 border-b border-b-gray-50">\n' +
+        '                                        <span class="text-gray-600 text-sm font-medium ml-2 truncate">' + projectsArr[index].title + '</span>\n' +
+        '                                </td>\n' +
+        '                                <td class="py-2 px-4 border-b border-b-gray-50">\n' +
+        '                                    <span class="text-[13px] font-medium text-gray-400">' + projectsArr[index].dueDate + '</span>\n' +
+        '                                </td>\n' +
+        '                            </tr>'
+}
+
+function fillTbodyEvents(index){
+    tbodyOfEvents.innerHTML += '<tr>\n' +
+        '                                <td class="py-2 px-4 border-b border-b-gray-50">\n' +
+        '                                        <span class="text-gray-600 text-sm font-medium ml-2 truncate">' + eventsArr[index].title + '</span>\n' +
+        '                                </td>\n' +
+        '                                <td class="py-2 px-4 border-b border-b-gray-50">\n' +
+        '                                    <span class="text-[13px] font-medium text-gray-400">' + eventsArr[index].date + '</span>\n' +
+        '                                </td>\n' +
+        '                            </tr>'
+}
+
+function fillTbodyMindMaps(index){
+    tbodyOfMindmaps.innerHTML += '<tr>\n' +
+        '                                <td class="py-2 px-4 border-b border-b-gray-50">\n' +
+        '                                        <span class="text-gray-600 text-sm font-medium ml-2 truncate">' + mindmapsArr[index].name + '</span>\n' +
+        '                                </td>\n' +
+        '                                <td class="py-2 px-4 border-b border-b-gray-50">\n' +
+        '                                    <span class="text-[13px] font-medium text-gray-400"></span>\n' +
+        '                                </td>\n' +
+        '                            </tr>'
+}
+
+function loadRecentProjectsIntoArrays() {
+    const user = auth.currentUser;
+
+    getDocs(projectsRef.orderBy("dueDate", "desc").limit(3))
+        .then(querySnapshot => {
+            projectsArr = []; // Clear the projects array before populating it again
+            querySnapshot.forEach(doc => {
+                const projectData = doc.data();
+                let dueDate = projectData.dueDate.toDate();
+                const project = { id: doc.id, ...projectData, dueDate: dueDate };
+                projectsArr.push(project);
+            });
+        })
+        .catch(error => {
+            console.error("Error loading projects: ", error);
+        });
+
+    getDocs(eventsRef.where("date", ">", new Date()).orderBy("date", "asc").limit(3))
+        .then(querySnapshot => {
+            eventsArr = []; // Clear the projects array before populating it again
+            querySnapshot.forEach(doc => {
+                const eventData = doc.data();
+                const event = { id: doc.id, ...eventData };
+                eventsArr.push(event);
+            });
+        })
+        .catch(error => {
+            console.error("Error loading events: ", error);
+        });
+
+    getDocs(mindmapsRef.limit(3))
+        .then(querySnapshot => {
+            mindmapsArr = []; // Clear the projects array before populating it again
+            querySnapshot.forEach(doc => {
+                const mindmapData = doc.data();
+                const event = { id: doc.id, ...mindmapData };
+                mindmapsArr.push(event);
+            });
+        })
+        .catch(error => {
+            console.error("Error loading events: ", error);
+        });
+
+}
+
+function loadAmounts(){
     const user = auth.currentUser;
     if (user) {
-        const projectsRef = collection(db, "users", user.uid, "projects");
-        const eventsRef = collection(db, "users", user.uid, "events");
-        const mindmapsRef = collection(db, "users", user.uid, "mindmaps");
 
-        const projectSnapshot = await getCountFromServer(projectsRef);
+        const projectSnapshot = getCountFromServer(projectsRef);
         amountOfProjects.innerHTML = projectSnapshot.data().count;
 
-        const eventSnapshot = await getCountFromServer(eventsRef);
+        const eventSnapshot = getCountFromServer(eventsRef);
         amountOfAppointments.innerHTML = eventSnapshot.data().count;
 
-        const mindmapSnapshot = await getCountFromServer(mindmapsRef);
+        const mindmapSnapshot = getCountFromServer(mindmapsRef);
         amountOfMindmaps.innerHTML = mindmapSnapshot.data().count;
-
     }
 
 }
