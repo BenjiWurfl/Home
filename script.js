7ea62ea48955087ef311476bd4e96da10736a32c
Noah-Pesendorfer <noah.pesendorfer@htl-wels.at>
2024-02-21 14:23:57 +0100

    Dashboard

--- script.js
+++ script.js
@@ -1,27 +1,53 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Setze den Vornamen des Benutzers
-    document.getElementById('user-firstname').textContent = 'Benjamin'; // Beispielname, ersetze dies durch den tatsächlichen Vornamen
+import {initializeApp} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js";
+import {
+    addDoc,
+    collection,
+    deleteDoc,
+    doc,
+    getDoc,
+    getDocs,
+    getFirestore,
+    updateDoc
+} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+import {getAuth, onAuthStateChanged} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
-    // Beispiel-Daten, diese sollten durch tatsächliche Daten aus Firestore ersetzt werden
-    const exampleEvents = ['Termin 1', 'Termin 2', 'Termin 3'];
-    const exampleProjects = ['Projekt 1', 'Projekt 2', 'Projekt 3'];
-    const exampleMindmaps = ['MindMap 1', 'MindMap 2', 'MindMap 3'];
+const firebaseConfig = {
+    apiKey: "AIzaSyBe7d9bllq8RnmI6xxEBk3oub3qogPT2aM",
+    authDomain: "thinkwise-c7673.firebaseapp.com",
+    databaseURL: "https://thinkwise-c7673-default-rtdb.europe-west1.firebasedatabase.app/",
+    projectId: "thinkwise-c7673",
+    storageBucket: "thinkwise-c7673.appspot.com",
+    messagingSenderId: "37732571551",
+    appId: "1:37732571551:web:9b90a849ac5454f33a85aa",
+    measurementId: "G-8957WM4SB7"
+};
 
-    // Funktion zum Einfügen von Daten in die Listen
-    function populateList(listId, items) {
-        const list = document.getElementById(listId);
-        items.forEach(item => {
-            const li = document.createElement('li');
-            li.textContent = item;
-            list.appendChild(li);
-        });
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth();
+let notesArr = [];
+let projectsArr = []
+let currentProject;
+let currentNote;
+let currentSortByState = "Date down";
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        loadAmounts();
+    } else {
+        console.log("No user is signed in.");
     }
-
-    // Daten in die Listen einfügen
-    populateList('calendar-list', exampleEvents);
-    populateList('projects-list', exampleProjects);
-    populateList('mindmaps-list', exampleMindmaps);
 });
+
+const amountOfProjects = document.querySelector('.num-of-Projects');
+const amountOfAppointments = document.querySelector('.num-of-Appointments');
+const amountOfMindmaps = document.querySelector('.num-of-Mindmaps');
+
+async function loadAmounts(){
+    const user = auth.currentUser;
+    const projectRef = collection(db, "users", user.id, "projects");
+    amountOfProjects.innerHTML = await projectRef.count().get();
+}
 
 //Navbar
 
